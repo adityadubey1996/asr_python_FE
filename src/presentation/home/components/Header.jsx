2cbc597f6f2347557201ddc028d60e5e23fb68c1
K S Likith <kslikith2002@gmail.com>
2024-05-09 21:14:25 +0530

    adds home screen

---
+++ src/presentation/home/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const user = useSelector((state) => state.user.user);
+
+  return (
+    <div className="p-5 sticky border h-20 border-b-gray-200 top-0">
+      <div className="flex justify-end">
+        <div className="flex items-center gap-6 justify-center">
+          <img
+            src={`${user.data.profile_picture}`}
+            className="rounded-full"
+            width={30}
+            alt="profile pic"
+          ></img>
+          <h1 className="font-bold ">{user.data.name}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+const constants = {
+    STATUS_PENDING:"pending",
+    STATUS_CANCELLED:"cancelled",
+    STATUS_APPROVE:"approve",
+    SIDEBAR_FILES:"My files",
+    SIDEBAR_DASHBOARD:"Dashboard",
+    SIDEBAR_TASK_MANAGER:"Task Manager",
+    SIDEBAR_SETTINGS:"Settings",
+    SIDEBAR_REFER_AND_EARN:"Refer & Earn"
+}
+
+export default constants
